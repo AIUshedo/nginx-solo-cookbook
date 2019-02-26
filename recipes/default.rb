22c125915552da308876890890dda47ca2633c72
unknown <aushedo@spartaglobal.com>
2019-02-26 15:28:34 +0000

    nginx done

---
+++ recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: nginx
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe "apt"
+package "nginx"
+
+service 'nginx' do
+  action :enable
+end
+
+service 'nginx' do
+  action :start
+end
+
+template "/etc/nginx/nginx.conf" do
+  source "nginx.conf.erb"
+  variables proxy_port: node["nginx"]["proxy_port"]
+  notifies :restart, "service[nginx]"
+end
---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook:: nginx
+# Spec:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'nginx::default' do
+  context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+
+
+it 'should install nginx' do
+     expect(chef_run).to install_package("nginx")
+   end
+
+ it 'nginx should be enabled' do
+       expect(chef_run).to enable_service("nginx")
+     end
+
+ it 'nginx should be started' do
+       expect(chef_run).to start_service("nginx")
+     end
+
+     it 'should create an nginx.conf template in etc/nginx' do
+      expect(chef_run).to create_template("/etc/nginx/nginx.conf").with_variables(proxy_port: 7000)
+    end
+end
+end
